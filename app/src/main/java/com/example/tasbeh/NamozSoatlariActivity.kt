c629be870293ababd91404c9ad206d2859c6ac5e
Behzod96447 <behzod234963@internet.ru>
2023-08-15 11:22:51 +0300

    Ilk qadam

--- app/src/main/java/com/example/tasbeh/NamozSoatlariActivity.kt
+++ app/src/main/java/com/example/tasbeh/NamozSoatlariActivity.kt
@@ -3,7 +3,9 @@ package com.example.tasbeh
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -20,18 +22,31 @@ class NamozSoatlariActivity: AppCompatActivity() {
     private fun initViews() {
 
         val webView:WebView=findViewById(R.id.wv_NamozSoatlari)
+        webView.settings.javaScriptEnabled
+
+        webView.webViewClient=object : WebViewClient(){
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url:String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+
+        }
+
         val swrSwipeToRefresh:SwipeRefreshLayout=findViewById(R.id.swr_SwipeToRefrsh)
         swrSwipeToRefresh.setColorSchemeColors(Color.BLACK)
         swrSwipeToRefresh.setOnRefreshListener {
 
-            webView.loadUrl("https://namozvaqtlari.com/uz/?ysclid=llauriyil987938031")
+            webView.loadUrl("https://namozvaqtlari.com/uz/namoz-vaqtlari/17-hiva-bugungi-namoz-vatlari.html")
             val text="Yangilandi"
             Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
             swrSwipeToRefresh.isRefreshing=false
 
         }
 
-        webView.loadUrl("https://namozvaqtlari.com/uz/?ysclid=llauriyil987938031")
+        webView.loadUrl("https://namozvaqtlari.com/uz/namoz-vaqtlari/17-hiva-bugungi-namoz-vatlari.html")
 
 
     }
