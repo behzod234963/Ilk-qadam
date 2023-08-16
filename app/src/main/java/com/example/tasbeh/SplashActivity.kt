d05dbacad4ef46bf33f12d20ffba84c4d5591dd9
Behzod96447 <behzod234963@internet.ru>
2023-08-16 13:04:42 +0500

    Ilk qadam

---
+++ app/src/main/java/com/example/tasbeh/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.tasbeh
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        initView()
+
+    }
+
+    private fun initView() {
+
+        Handler().postDelayed({
+
+            openMain()
+
+        },2000)
+
+    }
+    fun openMain(){
+
+        val intent=Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+}
